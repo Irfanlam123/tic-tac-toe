6ff04f9b3a9501e272aafd4f55ec14c5d484f436
Irfanlam123 <irfanalam010202@gmail.com>
2023-11-01 23:18:46 +0530

    first commit

---
+++ src/TicTacToeGames/Square.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Square = (props) => {
+  return (
+    <div 
+    onClick={props.onClick}
+    style={{ border:"5px solid black", height:'100px' ,font:"bold"
+    ,width:'100%',display:'flex'
+    ,justifyContent:'center',
+    alignItems:'center',
+    backgroundColor: "blue",fontSize:"80px",color:"red"
+
+    }}  
+        className='square'>
+        <h5>{props.value}</h5>
+    </div>
+  )
+}
+
+export default Square
